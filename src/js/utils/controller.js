eb3a6a6b302529432e5c275c5cabd0cecd2205b3
李怡欣 <liyixin@jd.com>
2019-01-17 16:46:44 +0800

    feat: 更新模版

--- src/js/utils/controller.js
+++ src/js/utils/controller.js
@@ -7,7 +7,7 @@ import {
   O2_AMBIENT_MAIN,
   O2_AMBIENT_CONFIG,
   O2_AMBIENT_CONFIG_KEY,
-  O2_AMBIENT_INIT
+  O2_AMBIENT_IS_CONFIG_RESET
 } from './const'
 import { getParameterByName } from './util'
 
@@ -17,7 +17,7 @@ const isShowController = getParameterByName('controller') // 是否展示控制�
 const isAmbientPlat = getParameterByName('platform') === '1' // 是否平台环境
 
 class Controller {
-  constructor() {
+  constructor () {
     this.config = window[O2_AMBIENT_CONFIG] || {}
     this.isShowController = isShowController
     this.isAmbientPlat = isAmbientPlat
@@ -34,10 +34,13 @@ class Controller {
     if (!this.isShowController && !this.isAmbientPlat) {
       this.setControllerHide()
     }
+
+    if (window[O2_AMBIENT_CONFIG_KEY]) return
+    window[O2_AMBIENT_CONFIG_KEY] = O2_AMBIENT_CONFIG
   }
 
   // 监听 postmessage
-  bindMsg() {
+  bindMsg () {
     window.addEventListener('message', (msg) => {
       if (msg.data.type !== 'reset') return
       window[O2_AMBIENT_CONFIG] = Object.assign(window[O2_AMBIENT_CONFIG], msg.data.data)
@@ -53,17 +56,17 @@ class Controller {
   }
 
   // 设置控制板层级
-  setGUIzIndex(zIndex) {
+  setGUIzIndex (zIndex) {
     this.gui.domElement.parentElement.style.zIndex = zIndex
   }
 
   // 设置页面背景色
-  setBackgroundColor(color) {
+  setBackgroundColor (color) {
     document.body.style.backgroundColor = color
   }
 
   // 传送数据
-  transMsg(dom) {
+  transMsg (dom) {
     let transWin = dom.contentWindow
     transWin.postMessage({
       type: 'reset',
@@ -72,7 +75,7 @@ class Controller {
   }
 
   // iframe dom
-  transferProcess() {
+  transferProcess () {
     if (!this.transferMonPC) {
       this.transferMonPC = document.getElementById('transferMon_pc')
     }
@@ -85,12 +88,12 @@ class Controller {
   }
 
   // 重置画布
-  resetCanvas(opts) {
+  resetCanvas (opts) {
     if (this.isAmbientPlat) {
       this.transferProcess()
-      
-      if (window[O2_AMBIENT_CONFIG_KEY]) return
-      window[O2_AMBIENT_CONFIG_KEY] = O2_AMBIENT_CONFIG
+
+      if (window[O2_AMBIENT_IS_CONFIG_RESET]) return
+      window[O2_AMBIENT_IS_CONFIG_RESET] = true
 
       return
     }
